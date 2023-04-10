1f037b2a3b0800a8e147bd7979756e18e21b13db
madesha23 <madesharamegowda11@gmail.com>
2023-04-10 15:14:09 +0530

    This is sum of natural number program

---
+++ Sum_natural.java
@@ -0,0 +1,14 @@
+public class Sum_natural
+{
+	public static void main(String args[])
+	{
+		int i, len=10, sum=0;
+
+		for(i=0; i<=len; i++)
+		{
+			sum = sum + i;
+
+		}
+		System.out.println("Sum of natural nummmbers: "+ sum);
+	}
+}
