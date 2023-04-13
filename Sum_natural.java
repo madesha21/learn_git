18373fc115470fe517e3173d3190784bcd3c2ddb
madesha21 <madesharamegowda11@gmail.com>
2023-04-13 14:52:32 +0530

    Added comments to the program

--- Sum_natural.java
+++ Sum_natural.java
@@ -1,14 +1,14 @@
-public class Sum_natural
+public class Sum_natural			// class name Sum_natural
 {
-	public static void main(String args[])
+	public static void main(String args[])	// program starts here
 	{
-		int i, len=10, sum=0;
+		int i, len=10, sum=0;	// initializing variables
 
-		for(i=0; i<=len; i++)
+		for(i=0; i<=len; i++)	// for loop
 		{
 			sum = sum + i;
 
 		}
-		System.out.println("Sum of natural nummmbers: "+ sum);
+		System.out.println("Sum of natural nummmbers: "+ sum);	//printing the statement
 	}
 }
